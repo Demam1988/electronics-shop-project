584602bbd42bb8113603ffdeec610a74c2e61bd1
Mamed Mamedov <demam777@gmail.com>
2023-07-24 00:05:35 +0300

    add

--- src/item.py
+++ src/item.py
@@ -1,9 +1,15 @@
+import csv
+import os
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
     pay_rate = 1.0
     all = []
+    PATH_DIR = os.path.abspath('../src/')
+    PATH_CSV = os.path.join(PATH_DIR, 'items.csv')
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
@@ -13,7 +19,24 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.__name = name
+        self.price = price
+        self.quantity = quantity
+        self.__class__.all.append(self)
+
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) < 10:
+            self.__name = name
+        else:
+            self.__name = name[:10]
+
+
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +44,28 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        total_cost = self.price * self.quantity
+        return total_cost
+
 
     def apply_discount(self) -> None:
         """
-        Применяет установленную скидку для конкретного товара.
-        """
-        pass
+       Применяет установленную скидку для конкретного товара.
+       """
+        discoun = self.price * self.pay_rate
+        self.price = discoun
+
+    @classmethod
+    def instantiate_from_csv(cls, path=PATH_CSV):
+        with open(path, newline='', encoding='1251') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for x in reader:
+                cls(x["name"], x["price"], x["quantity"])
+
+
+    @staticmethod
+    def string_to_number(str_num):
+        return int(float(str_num))
+
+
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,30 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+import pytest
+
+def test_calculate_total_price():
+    """
+    Рассчитываем Общая стоимость товара
+    """
+    price = 10
+    quantity = 10
+    total_cost = price * quantity
+    assert total_cost == 100
+
+def test_apply_discount():
+    """
+    Применяет установленную скидку
+    """
+    price = 100000
+    pay_rate = 0.8
+    price = price * pay_rate
+    assert price == 80000.0
+
+@pytest.fixture()
+def item():
+    return Item("Bob", 1000, 5)
+
+def test_getters(item):
+    assert item.name == "Bob"
+    assert item.price == 1000
+    assert item.quantity == 5
