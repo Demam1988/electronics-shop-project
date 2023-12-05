74943fb9b11b8e77bae45ea4abd867d1f1eb2efb
Mamed Mamedov <demam777@gmail.com>
2023-12-05 20:50:50 +0300

    add

--- homework-5/main.py
+++ homework-5/main.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     kb.change_lang()
     assert str(kb.language) == "RU"
 
-    # Сделали RU -> EN -> RU
+    # Сделали EN -> RU -> EN
     kb.change_lang()
     assert str(kb.language) == "EN"
 
