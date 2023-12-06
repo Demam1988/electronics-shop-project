9073348924b9b9117a4bed9d669a68862a6324e8
Mamed Mamedov <demam777@gmail.com>
2023-12-06 09:52:13 +0300

    add

--- homework-5/main.py
+++ homework-5/main.py
@@ -10,8 +10,8 @@ if __name__ == '__main__':
     assert str(kb.language) == "RU"
 
     # Сделали EN -> RU -> EN
-    kb.change_lang()
-    assert str(kb.language) == "EN"
+    kb.change_lang().change_land()
+    assert str(kb.language) == "RU"
 
     kb.language = 'CH'
     # AttributeError: property 'language' of 'Keyboard' object has no setter
